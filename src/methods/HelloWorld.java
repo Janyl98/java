2fa0dfb9c296a2d97567b1ec472f53f83ec6891f
Zhanyl Kubatbek kyzy <kubatbekova.janyl98@gmail.com>
2025-05-04 21:58:07 +0200

    methods

---
+++ src/methods/HelloWorld.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class HelloWorld {
+
+    public static void greetUser(String name) {
+        System.out.println("Салам, " + name + "!");
+    }
+
+
+    public static void main(String[] args) {
+        greetUser("Ainura");  // Чыгуу: Салам, Ainura!
+    }
+}
---
+++ src/methods/Twonumber.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class Twonumber {
+    public static int max(int x, int y) {
+        return (x > y) ? x : y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(5, 9));  // Чыгуу: 9
+    }
+}
